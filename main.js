452d6caf1a33595d05301fc80ae43098d86fc2ee
Sascha Andeweg <sascha@designed.irish>
2020-02-09 15:56:26 +0000

    commit of the pagination video

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const baseURL = "https://swapi.co/api/";
-
-function getData(type, cb){
+function getData(url, cb){
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function(){
@@ -8,7 +6,7 @@ function getData(type, cb){
             cb(JSON.parse(this.responseText));
         }
     };
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 }
 
@@ -22,11 +20,26 @@ function getTableHeaders(obj){
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev){
+    if (next && prev){
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev){
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev){
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
 
-    getData(type, function(data){
+    getData(url, function(data){
+        var pagination;
+        if (data.next || data.previous){
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -40,6 +53,6 @@ function writeToDocument(type) {
             tableRows.push(`<tr>${dataRow}</tr>`)
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
